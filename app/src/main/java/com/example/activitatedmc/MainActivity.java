b8e49f12e4473641854a9488b1c37c8caba63512
Anamaria Balacescu <anamariabalacescu@yahoo.com>
2025-02-25 11:50:16 +0200

    Lab2: Edit text cu suma

--- app/src/main/java/com/example/activitatedmc/MainActivity.java
+++ app/src/main/java/com/example/activitatedmc/MainActivity.java
@@ -8,7 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isDefaultText = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void changeText(View view) {
+        TextView textView = findViewById(R.id.textView);
+
+        EditText edit1 = findViewById(R.id.editTextText);
+        EditText edit2 = findViewById(R.id.editTextText2);
+
+        int num1 = 0;
+        try {
+            num1 = Integer.parseInt(edit1.getText().toString());
+        } catch(NumberFormatException nfe) {
+            System.out.println("Could not parse " + nfe);
+        }
+
+        int num2 = 0;
+        try {
+            num2 = Integer.parseInt(edit2.getText().toString());
+        } catch(NumberFormatException nfe) {
+            System.out.println("Could not parse " + nfe);
+        }
+
+        int sum = num1 + num2;
+
+        textView.setText(String.valueOf(sum));
+
     }
 }
