3eb5f5f04849d500146cb68615daed8ec9dca0c5
Anamaria Balacescu <anamariabalacescu@yahoo.com>
2025-04-27 18:25:41 +0300

    Created a new project

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Tehnici_actuale_motoare_anti_malware"
+include(":app")
+ 
