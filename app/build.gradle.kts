f9904cd43897f22885ac04c4efe6db4af77984f6
Anamaria Balacescu <anamariabalacescu@yahoo.com>
2025-05-19 21:51:46 +0300

    final project update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -12,7 +13,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -26,7 +26,6 @@ android {
         }
     }
 
-    // Java 11 bytecode
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -34,18 +33,40 @@ android {
 }
 
 dependencies {
-    // --- Dependențe de bază AndroidX ---
+    // AndroidX
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    // Networking & UI libs
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.github.PhilJay:MPAndroidChart:v3.0.2")
+    implementation("com.github.PhilJay:MPAndroidChart:3.0.2")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
-    testImplementation(libs.junit)               // 'junit:junit:4.13.2'
-    androidTestImplementation(libs.ext.junit)    // 'androidx.test.ext:junit:1.1.5'
-    androidTestImplementation(libs.espresso.core)// 'androidx.test.espresso:espresso-core:3.5.1'
+    // Firebase BOM (manages versions for all Firebase libs)
+    implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
+
+// Firebase BOM
+    implementation(platform("com.google.firebase:firebase-bom:32.1.1"))
+// Realtime Database KTX
+    implementation("com.google.firebase:firebase-database-ktx")
+
+    // Google Sign-In
+    implementation("com.google.android.gms:play-services-auth:20.5.0")
+
+    // Firebase Auth (you should already have this)
+//    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
